f61723124e4ea03733343bb4931edf054ae898cc
Hakkun <hammad.ak96@gmail.com>
2024-09-19 16:32:20 +0800

    updating Bookfile code

--- BookstoreApplication.py
+++ BookstoreApplication.py
@@ -7,13 +7,25 @@ Reference idea credit to Youtuber Bek Brace with Video title 'Build a CLI Applic
 
 """
 
-booklist = []
-
 def main():
-    choice = 0
+    try:
+        booklist = [] #Blank list to be appended
+        INfile = open("Booklists.txt", "r")
+        line = INfile.readline()
+        while line:
+            booklist.append(line.rstrip("\n").split(","))
+            line = INfile.readline()
+        INfile.close()
+    except FileNotFoundError: #If txt file is not there(accidentaly deleted)
+        print("The <booklist.txt> file is not FOUND")
+        print("Starting new booklists...")
+        booklist = [] # reinitialize the list
+
+    choice = 0 #initialize starting
     while choice != 4:
         print("1)Add a book\n2)Search your book\n3)Display all books\n4)Close")
         choice = int(input("Choose which number: "))
+        #Making Choice
         if choice == 1:
             print("Adding a book...")
             nBook = input("Name of book: ")
@@ -21,17 +33,21 @@ def main():
             nPages = input("Number of pages: ")
             booklist.append([nBook, nAuthor, nPages])
             print("Book added to list")
+            outfile = open("Booklists.txt", "w")
+            for book in booklist: #saving to external file TXT
+                outfile.write(",".join(book) + "\n")
+            outfile.close()
         
         elif choice == 2:
-            if len(booklist) == 0:
-                print("No book in the list yet...\nPlease add a book first")
+            keyword = input("Input the keyword(Title, Author): ")
             for book in booklist:
-                keyword = input("Input the keyword(Title, Author): ")
                 if keyword in book:
                     print(str(book) + " is in the list")
         
         elif choice == 3:
-            print(booklist)
+            print("Displaying all books...")
+            print(booklist) #I don't quite get it the used of range(len(booklist)) print booklist[i] by the youtuber which print the same result if I just do this
+
         elif choice == 4:
             print("Closing the application...")
     print("Exit the app")
